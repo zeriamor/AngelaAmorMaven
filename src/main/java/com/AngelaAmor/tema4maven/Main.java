2edd489425fcfe13c0bea5930dfd997fc450afac
Ángela Amor <angamoram@alu.edu.gva.es>
2026-02-02 19:00:19 +0100

    Primer commit-Proyecto Maven

---
+++ src/main/java/com/AngelaAmor/tema4maven/Main.java
@@ -0,0 +1,17 @@
+package com.AngelaAmor.tema4maven;
+
+//TIP Para <b>ejecutar</b> el código, pulsar <shortcut actionId="Run"/> o
+// Haz clic en el ícono <icon src="AllIcons.Actions.Execute"/> del margen.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Pulsa <shortcut actionId="ShowIntentionActions"/> con tu cursor en el texto resaltado
+        // para ver cómo IntelliJ IDEA sugiere corregirlo.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Presione <shortcut actionId="Debug"/> para iniciar la depuración de su código. Hemos creado un punto de interrupción <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
+            // para ti, pero siempre puedes añadir más pulsando <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
